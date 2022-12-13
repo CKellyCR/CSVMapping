5339d685a2ad730656a891bd979469bc1666c5a3
CiaranKelly <ckelly@clearrisk.com>
2022-12-13 15:50:24 -0330

    Tuesday pm

--- map.js
+++ map.js
@@ -23,16 +23,6 @@ const loadCSV = (path) => {
     })
 }
 
-// const loadClaims = (path) =>{
-//     const res = [];
-//     return new Promise((resolve, reject)=>{
-//         fs.createReadStream(path)
-//         .pipe(csv())
-
-//     })
-
-// }
-
     /*
     dropDowns contains a list of objects, which contain a name and an array.
     ex:
@@ -43,7 +33,7 @@ const loadCSV = (path) => {
     }
     */
 
-const handleMapping = async (claimsPath, valuesPath) => {
+const handleMapping = async (claimsPath, valuesPath) => {//path variables are set at the end of the code (both csv files)
     const claims = await loadCSV(claimsPath);
     const dropDown = await loadCSV(valuesPath);
 
@@ -51,18 +41,20 @@ const handleMapping = async (claimsPath, valuesPath) => {
 
     const addMapping = (field, value) => {
         let mapping = mappings.find(x => x.name.toLowerCase() === field.toLowerCase());
+        //let mapping = mappings.find(x => x.name.match() === field.match());
+
         if (mapping !== undefined) {
             if (mapping?.values === undefined) {
                 mapping.values = [];
-            }
+            };
             mapping.values.push(value);
         } else {
             mappings.push({
                 name: field
             })
             addMapping(field,value);
-        }
-    }
+        };
+    };
 
     const getMapping = (field, key) => {
         let mapping = mappings.find(x => x.name.toLowerCase() == field.toLowerCase());
@@ -70,8 +62,8 @@ const handleMapping = async (claimsPath, valuesPath) => {
             let potentialValue = mapping.values.find(x => x.Key === key);
             if (potentialValue !== undefined) {
                 return potentialValue.Value;
-            }
-        }
+            };
+        };
 
         return undefined;
     }
@@ -80,10 +72,14 @@ const handleMapping = async (claimsPath, valuesPath) => {
         addMapping(dd.FIELD_NAM, {
             Key: dd.CODE_ID,
             Value: dd.CODE_DESC
-        })
-    }
+        });
+    };
 
-    let mapping = getMapping('Carrier', 'ACE');
+    // for (let dd of dropDown){
+    //     addMapping(dd.)
+    // }
+
+    let mapping = getMapping('SpecialAnalysis#106', '487550');
     console.log(mapping);
 
     for (let claim of claims) {
